722ba662f1f8f962d90b52e3f2627e2151e15ade
j08lue <j08lue@gmail.com>
2020-05-20 13:41:23 +0200

    semantic versioning and original description

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='loess',
-    version='v1',
-    description='Loess interpolation for s2 regular timeseries',
-    author='Nicolai Thomassen',
-    author_email='nith@dhigroup.com',
+    name="pyloess",
+    version="v0.1.0",
+    description="A simple implementation of the LOESS algorithm using numpy based on NIST",
+    author="Nicolai Thomassen",
+    author_email="nith@dhigroup.com",
     packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=[],
-    extras_require={    }
+    python_requires=">=3.6",
 )
