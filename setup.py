48de2f0c8a28f1fb4f5cf0248f7a9fa37d2b23e4
Nicolai Thomassen <49398380+NicolaiThomassen@users.noreply.github.com>
2020-05-20 13:43:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name="pyloess",
     version="v0.1.0",
     description="A simple implementation of the LOESS algorithm using numpy based on NIST",
-    author="Nicolai Thomassen",
-    author_email="nith@dhigroup.com",
+    author="joaofig",
     packages=find_packages(),
     python_requires=">=3.6",
 )
