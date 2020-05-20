56630fe7dd479af915fa4c02a1bedbe039a8a073
j08lue <j08lue@gmail.com>
2020-05-20 14:05:55 +0200

    real name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="pyloess",
     version="v0.1.0",
     description="A simple implementation of the LOESS algorithm using numpy based on NIST",
-    author="joaofig",
+    author="João Paulo Figueira",
     auther_email="joaofig@github.com",
     packages=find_packages(),
     python_requires=">=3.6",
